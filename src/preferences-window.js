5857c60bdf6feefb92810635903cbdd75a82493a
c-bata <contact@c-bata.link>
2016-10-16 02:11:46 +0900

    Add preferences window

--- src/preferences-window.js
+++ src/preferences-window.js
@@ -5,8 +5,8 @@ var win = null
 
 function createWindow () {
     win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 500,
+        height: 400,
         resizable: true
     })
     win.loadURL(`file://${__dirname}/preferences.html`)
